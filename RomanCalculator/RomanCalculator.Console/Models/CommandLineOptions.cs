6bc275d85693050df6f91380d647e8cad6168904
vludlsswithdevelop <vladislav.mikkoev@yandex.ru>
2023-02-18 11:56:18 +0300

    docs: hello message fix

--- RomanCalculator/RomanCalculator.Console/Models/CommandLineOptions.cs
+++ RomanCalculator/RomanCalculator.Console/Models/CommandLineOptions.cs
@@ -6,7 +6,7 @@ namespace RomanCalculator.Console.Models
     {
         [Option('e', "expr", Required = true, HelpText = "Input roman expression.", Separator = ' ')]
         public string RomanExpression { get; set; }
-        [Option('w', Default = false, Required = false, HelpText = "Input '-w', if you want continue to work with the app")]
+        [Option('w', Default = false, Required = false, HelpText = "Input '-w', if you want continue to work with the app.")]
         public bool WorkContinue { get; set; }
     }
 }
